167feab7116f07181652d3135bf3a75b51c81955
richeeee128 <qoqw808@gmail.com>
2024-11-09 23:20:43 +0900

    feat : JS - home

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,13 +1,19 @@
 //Header에 페이지 아래로 스크롤시 다크 스타일링 적용
-function scrollHeader() {
+document.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
   const headerHeight = header.getBoundingClientRect().height;
-  if (window.scrollY > headerHeight) {
+  const scrollY = window.scrollY;
+  if (scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
-  header.style.backgoundColor = `green`;
-}
+});
 
-document.addEventListener("scroll", scrollHeader);
+//스크롤 시 Home 섹션을 투명하게 처리함
+document.addEventListener("scroll", () => {
+  const home = document.querySelector(".home__container");
+  const homeRect = home.getBoundingClientRect();
+  const homeHeight = homeRect.height;
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
