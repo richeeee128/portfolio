dc1dee4e578cadcbfe1fe3b04091d10f35dca8ee
richeeee128 <qoqw808@gmail.com>
2024-11-20 12:00:25 +0900

    JS-projects

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //프로젝트 필터링
+const projectsContainer = document.querySelector(".projects");
 const categories = document.querySelector(".categories");
 const projects = document.querySelectorAll(".project");
 categories.addEventListener("click", (event) => {
@@ -9,6 +10,18 @@ categories.addEventListener("click", (event) => {
     return;
   }
 
+  handleActiveSelection(event.target);
+  filterProjects(fillter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(fillter) {
+  projectsContainer.classList.add("anim-out");
   projects.forEach((project) => {
     if (fillter === "all" || fillter === project.dataset.type) {
       project.style.display = "block";
@@ -16,4 +29,7 @@ categories.addEventListener("click", (event) => {
       project.style.display = "none";
     }
   });
-});
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}
